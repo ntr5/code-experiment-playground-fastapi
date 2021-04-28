7b95ee6175cc1d25d4e91080b3f37004e22463a6
Nathan Storrs <pressntr@gmail.com>
2021-04-27 21:42:01 -0600

    add test_new_todo_raises_not_authenticated

--- tests/test_create_todo.py
+++ tests/test_create_todo.py
@@ -21,3 +21,20 @@ def test_create_new_todo(login, client):
     assert body['complete'] == False
     assert body['id'] == 1
     assert body['owner_id'] == 1
+
+
+#TODO: test_creaet_new_todo_raises_not_authenticated
+@pytest.mark.todos
+def test_creaet_new_todo_raises_not_authenticated(client):
+    # Arrange
+    url = '/create-todo'
+    payload = {
+        "title": "Buy Milk",
+        "complete": False
+    }
+    # Act
+    response = client.post(url, json=payload)
+    body = response.json()
+    # Assert
+    assert response.status_code == 401
+    assert body['detail'] == 'Not authenticated'
