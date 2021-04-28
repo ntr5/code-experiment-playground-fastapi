a56b927f593273a00667d818b0890b9ac9752f78
Nathan Storrs <pressntr@gmail.com>
2021-04-27 19:55:34 -0600

    add test_login_raises_incorrect_password

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -19,7 +19,7 @@ def test_login(test_db_session, create_single_user, client):
 
 
 @pytest.mark.auth
-def test_login_raises_incorrect_username(test_db_session, client):
+def test_login_raises_incorrect_username(client):
     # Arrange
     url = '/login'
     payload = {
@@ -32,3 +32,19 @@ def test_login_raises_incorrect_username(test_db_session, client):
     # Assert
     assert response.status_code == 404
     assert body['detail'] == 'Incorrect Username'
+
+
+@pytest.mark.auth
+def test_login_raises_incorrect_password(create_single_user, client):
+    # Arrange
+    url = '/login'
+    payload = {
+        "username": "test1",
+        "password": "wrong password"
+    }
+    # Act
+    response = client.post(url, data=payload)
+    body = response.json()
+    # Assert
+    assert response.status_code == 404
+    assert body['detail'] == 'Incorrect Password'
